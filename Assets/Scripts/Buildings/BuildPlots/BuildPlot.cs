448fcb8253d9b86802fd10388d1a136d7687b153
CharlieHewitt <33226685+CharlieHewitt@users.noreply.github.com>
2020-05-11 21:44:34 +0100

    added build plot (model)

---
+++ Assets/Scripts/Buildings/BuildPlots/BuildPlot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPlot
+{
+    public BuildingType buildingType { get; private set; }
+    public Vector3 position { get; private set; }
+
+    public BuildPlot(Vector3 pos)
+    {
+        buildingType = BuildingType.NONE;
+        position = pos;
+    }
+
+    public void Build(BuildingType type)
+    {
+        if (buildingType != BuildingType.NONE)
+        {
+            Debug.Log(string.Format("Can't build {0}. There is already a building on this plot ({1})", type, buildingType));
+            return;
+        }
+
+        // update state
+        buildingType = type;
+    }
+
+    public void Demolish(BuildingType type)
+    {
+        if (buildingType == BuildingType.NONE)
+        {
+            Debug.Log("Can't demolish. There is no building on this plot.");
+        }
+
+        // update state
+        buildingType = BuildingType.NONE;
+    }
+
+    public string StatusString()
+    {
+        return (string.Format("Build plot information:\nPosition: ({0},{1},{2}) Building: {3}", position.x, position.y, position.z, buildingType));
+    }
+
+}
